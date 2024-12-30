95d0282596c9390f203a22d8d43994234d1f79c3
Yasin Yücetaş <yasinyucetas2@gmail.com>
2024-12-30 21:52:23 +0300

    x

---
+++ 30AralikTest.dart
@@ -0,0 +1,12 @@
+void main() {
+  List <String> gunler=["Pazartesi",
+                       "Salı",
+                       "Çarşamba",
+                       "Perşembe",
+                       "Cuma",
+                       "Cumartesi",
+                       "Pazar"];
+  print(gunler[2]);
+  print("Günler listesinin 3.elemanı: ${gunler[2]}");
+  }
+}
